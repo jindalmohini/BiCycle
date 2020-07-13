9bc76950996b500ccb6b160109fe78ee4df186b1
Mohini <jindalmohiniagrawal@gmail.com>
2020-07-13 06:54:20 +0530

    create wheel class under Gear class So that it can Work independentaly and wheel have a Method which calculate diameter of a wheel

--- BicycleProblem.java
+++ BicycleProblem.java
@@ -1,29 +1,67 @@
 class Gear
 {
-    int chainRing,cog;
-    double ratio;
-    Gear(int chainRing,int cog)
+   private int chainRing,cog;
+    private double ratio;
+    wheel w;
+    class wheel{
+        private double rim,tyre;
+        wheel(double rim,double tyre)
+        {
+            this.rim=rim;
+            this.tyre=tyre;
+        }
+        public double diameter()
+        {
+            return rim+(2*tyre);
+        }
+    }
+    Gear(int chainRing,int cog,double rim,double tyre)
     {
+         w=new wheel(rim,tyre);
         this.chainRing=chainRing;
         this.cog=cog;
     }
-    double getRatio()
+    Gear(int chainRing,int cog){
+        this.chainRing=chainRing;
+        this.cog=cog;
+    }
+   public double getRatio()
     {
         ratio=(double)chainRing/cog;
         return ratio;
     }
+    public double gearInches()
+    {
+        this.getRatio();
+        return ratio*w.diameter();
+    }
 }
+
+
+
 public class BicycleProblem
 {
-    public static void playWithGear()
+    public static void playWithGearRatio()
     {
         Gear big=new Gear(52,11);
         Gear small=new Gear(30,27);
         System.out.println("Ratio Of Big Gear="+big.getRatio());
         System.out.println("Ratio of Small Gear="+small.getRatio());
     }
+
+     public static void playWithGearInches()
+    {
+        Gear g1=new Gear(52,11,26,1.5);
+        Gear g2=new Gear(52,11,24,1.25);
+        System.out.println("Gear Inches for Gear1="+g1.gearInches());
+        System.out.println("Gear Inches for Gear2="+g2.gearInches());
+    }
+
     public static void main(String args[])
     {
-        playWithGear();
+        System.out.println("function: playWithGearRatio");
+        playWithGearRatio();
+        System.out.println("function:playWithGearInches");
+        playWithGearInches();
     }
 }
