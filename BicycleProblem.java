1fe5939295b29b0f5a8bdec459c8d211d5f7d1b5
Mohini <jindalmohiniagrawal@gmail.com>
2020-07-13 07:52:27 +0530

    we refferd wheel class frm the initialisation itself and provide diameter as a method in Gear class. so that gearInches method know Nthing about wheel class and thus this method is independent of wheel class

--- BicycleProblem.java
+++ BicycleProblem.java
@@ -1,23 +1,29 @@
+
 class Gear
 {
    private int chainRing,cog;
     private double ratio;
-    wheel w;
+    wheel wheel;
    
-    Gear(int chainRing,int cog)
+    Gear(int chainRing,int cog,wheel wheel)
     {
         this.chainRing=chainRing;
         this.cog=cog;
+        this.wheel=wheel;
     }
    public double getRatio()
     {
         ratio=(double)chainRing/cog;
         return ratio;
     }
-    public double gearInches(wheel w)
+    public double gearInches()
     {
         this.getRatio();
-        return ratio*w.diameter();
+        return ratio*diameter();
+    }
+    public double diameter()
+    { 
+       return wheel.diameter();
     }
 }
  class wheel{
@@ -41,28 +47,28 @@ class Gear
 
 public class BicycleProblem
 {
-    public static void playWithGearRatio()
+  /*  public static void playWithGearRatio()
     {
         Gear big=new Gear(52,11);
         Gear small=new Gear(30,27);
         System.out.println("Ratio Of Big Gear="+big.getRatio());
         System.out.println("Ratio of Small Gear="+small.getRatio());
-    }
+    }*/
 
      public static void playWithGearInches()
     {
-        Gear g1=new Gear(52,11);
-        wheel w1=new wheel(26,1.5);
-        System.out.println("Diameter of tyre="+w1.diameter());
-        System.out.println("circumference of wheel="+w1.circumference());
-        System.out.println("Gear Inches for Gear1="+g1.gearInches(w1));
+        Gear g1=new Gear(52,11,(new wheel(26,1.5)));
+       // wheel w1=new wheel(26,1.5);
+       // System.out.println("Diameter of tyre="+w1.diameter());
+        //System.out.println("circumference of wheel="+w1.circumference());
+        System.out.println("Gear Inches for Gear1="+g1.gearInches());
         
     }
 
     public static void main(String args[])
     {
-        System.out.println("function: playWithGearRatio");
-        playWithGearRatio();
+       // System.out.println("function: playWithGearRatio");
+        //playWithGearRatio();
         System.out.println("function:playWithGearInches");
         playWithGearInches();
     }
