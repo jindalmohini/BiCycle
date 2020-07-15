3bd675b1f47e9a7cc18e1cbb477f772762eff601
Mohini <jindalmohiniagrawal@gmail.com>
2020-07-15 08:32:02 +0530

    generate if clause in spare method to see if the style is mountain or road,this style is initialised by main method. but i think this design is wrong because main method knows what to do of other class

--- SetBicycle.java
+++ SetBicycle.java
@@ -1,28 +1,38 @@
 class BiCycle
 {
-    private String size,tape_color,chain,tire_size;
-    public BiCycle(String size,String tape_color)
+    private String size,tape_color,chain,tire_size,style,front_shock,rear_shock;
+    public BiCycle(String style,String size,String tape_color)
     {
         this.size=size;
         this.tape_color=tape_color;
+        this.style=style;
+    }
+    public BiCycle(String style,String size,String front_shock,String rear_shock)
+    {
+        this.size=size;
+        this.style=style;
+        this.front_shock=front_shock;
+        this.rear_shock=rear_shock;
     }
     public void setSpares()
     {
-        chain="10-speed";
-        tire_size="23";
+        if(style.equals("mountain"))
+        {
+         System.out.println(" chain=10-speed"+"\n tire_size=2.1"+"\n rear_shock:"+this.rear_shock);
+        }
+        else
+        System.out.println(" chain=10-speed"+"\n tire_size=23"+"\n tape_color:"+this.tape_color);
+        
     }
-    public void display()
-    {
-        System.out.println("size="+this.size+"\n Tape_Color="+this.tape_color+"\n chain="+this.chain+"\n tire_size="+this.tire_size);
-}
+   
+
 
 }
 public class SetBicycle
 {
      public static void main(String args[])
     {
-        BiCycle bike=new BiCycle("M","red");
+        BiCycle bike=new BiCycle("mountain","S","MAintou","Fox");
         bike.setSpares();
-        bike.display();
     }
 }
